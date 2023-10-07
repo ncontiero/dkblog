d10c40296ff90e11ef103c21d430a4c46532092c
DKSHS <88355080+dkshs@users.noreply.github.com>
2023-10-07 12:25:10 -0300

    feat: adding SEO to the site and variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,49 @@ import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import { Inter } from "next/font/google";
 
+import { SITE_BASEURL, SITE_LOCALE, SITE_NAME } from "@/utils/constants";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "DkBlog",
+  metadataBase: new URL(SITE_BASEURL),
+  title: {
+    default: SITE_NAME,
+    template: `%s • ${SITE_NAME}`,
+  },
   description: "A blog using Next.Js.",
+  alternates: {
+    canonical: "/",
+  },
+  manifest: `${SITE_BASEURL}/manifest.json`,
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  openGraph: {
+    title: {
+      default: SITE_NAME,
+      template: `%s • ${SITE_NAME}`,
+    },
+    description: "A blog using Next.Js.",
+    siteName: SITE_NAME,
+    type: "website",
+    url: "/",
+    locale: SITE_LOCALE,
+  },
+  twitter: {
+    title: {
+      default: SITE_NAME,
+      template: `%s • ${SITE_NAME}`,
+    },
+    description: "A blog using Next.Js.",
+    card: "summary",
+  },
 };
-
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
