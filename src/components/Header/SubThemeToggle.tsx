d6b3e731d8d080dabe92845735e3e8c98b7e1c5c
DKSHS <88355080+dkshs@users.noreply.github.com>
2023-10-28 11:44:10 -0300

    fix: missing ref error

--- src/components/Header/SubThemeToggle.tsx
+++ src/components/Header/SubThemeToggle.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import {
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+  forwardRef,
+} from "react";
 import { useTheme } from "next-themes";
 
 import {
@@ -7,11 +12,18 @@ import {
   DropdownMenuItem,
 } from "@/components/ui/DropdownMenu";
 
-export function SubModeToggle() {
+export const SubModeToggle = forwardRef<
+  ElementRef<typeof DropdownMenuSubContent>,
+  ComponentPropsWithoutRef<typeof DropdownMenuSubContent>
+>(({ className, ...props }, ref) => {
   const { setTheme } = useTheme();
 
   return (
-    <DropdownMenuSubContent className="z-[9999]">
+    <DropdownMenuSubContent
+      className={"z-[9999]" + className}
+      ref={ref}
+      {...props}
+    >
       <DropdownMenuItem onClick={() => setTheme("light")}>
         Light
       </DropdownMenuItem>
@@ -21,4 +33,6 @@ export function SubModeToggle() {
       </DropdownMenuItem>
     </DropdownMenuSubContent>
   );
-}
+});
+
+SubModeToggle.displayName = "SubModeToggle";
