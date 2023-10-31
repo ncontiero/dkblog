2d0a7d6030891d65386bccae5b3f250c5fbc1a87
DKSHS <88355080+dkshs@users.noreply.github.com>
2023-10-31 16:34:14 -0300

    update: removing clockSkewInMs in clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/t(.*)", "/:user(.*)", "/api(.*)"],
-  clockSkewInMs: 3000,
 });
 
 export const config = {
