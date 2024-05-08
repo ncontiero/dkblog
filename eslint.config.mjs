fa85e2d16af3e5e9d943cd08208ee2b18d52872e
dkshs <88355080+dkshs@users.noreply.github.com>
2024-05-08 14:42:41 -0300

    chore: ignore some unsupported features

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,16 @@
 import { dkshs } from "@dkshs/eslint-config";
 
 export default dkshs({
+  javascript: {
+    overrides: {
+      "node/no-unsupported-features/node-builtins": [
+        "error",
+        {
+          ignores: ["fetch", "Blob", "File", "FormData", "Response", "Request"],
+        },
+      ],
+    },
+  },
   react: {
     overrides: {
       // https://github.com/jsx-eslint/eslint-plugin-react/issues/3717
