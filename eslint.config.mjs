88cc9580a0f62e139244ef7032a462385f552ab5
dkshs <88355080+dkshs@users.noreply.github.com>
2024-08-02 12:47:55 -0300

    chore(deps): update @dkshs/eslint-config to v2.8.3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,10 +11,4 @@ export default dkshs({
       ],
     },
   },
-  react: {
-    overrides: {
-      // https://github.com/jsx-eslint/eslint-plugin-react/issues/3717
-      "react/boolean-prop-naming": "off",
-    },
-  },
 });
