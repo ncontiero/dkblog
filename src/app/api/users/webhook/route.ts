cddf923d3bda7c92da132de4f405235ea9379451
Nicolas Contiero <git@ncontiero.com>
2025-04-03 15:41:51 -0300

    feat: use clerk verifyWebhook func

--- src/app/api/users/webhook/route.ts
+++ src/app/api/users/webhook/route.ts
@@ -1,53 +1,13 @@
-import type { WebhookEvent } from "@clerk/nextjs/server";
-
 import { randomBytes } from "node:crypto";
-import { headers } from "next/headers";
-import { Webhook } from "svix";
-import { env } from "@/env";
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { prisma } from "@/lib/prisma";
 import { errorResponse } from "@/utils/errorResponse";
 
 export async function POST(req: Request) {
-  const WEBHOOK_SECRET = env.CLERK_WEBHOOK_SIGNING_SECRET;
-
-  // Get the headers
-  const headerPayload = await headers();
-  const svixId = headerPayload.get("svix-id");
-  const svixTimestamp = headerPayload.get("svix-timestamp");
-  const svixSignature = headerPayload.get("svix-signature");
-
-  // If there are no headers, error out
-  if (!svixId || !svixTimestamp || !svixSignature) {
-    const errMessages = "Error occurred -- no svix headers";
-    return errorResponse(errMessages, [errMessages]);
-  }
-
-  // Get the body
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
-
-  // Create a new SVIX instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
-
-  let evt: WebhookEvent;
-
-  // Verify the payload with the headers
   try {
-    evt = wh.verify(body, {
-      "svix-id": svixId,
-      "svix-timestamp": svixTimestamp,
-      "svix-signature": svixSignature,
-    }) as WebhookEvent;
-  } catch (error) {
-    console.error("Error verifying webhook:", error);
-    const errMessages = "Error occurred -- could not verify webhook";
-    return errorResponse(errMessages, [errMessages]);
-  }
+    const evt = await verifyWebhook(req);
 
-  // Get the ID and type
-  const eventType = evt.type;
-  try {
-    switch (eventType) {
+    switch (evt.type) {
       case "user.created":
       case "user.updated": {
         const username =
