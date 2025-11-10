adb3b3ff022e76b9172f47dca9b730411c5cc9a3
Nicolas Contiero <git@ncontiero.com>
2025-11-10 12:58:15 -0300

    chore(eslint): remove prisma ignore and tailwindcss overrides from config

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,12 @@
 import { ncontiero } from "@ncontiero/eslint-config";
 
 export default ncontiero({
-  ignores: ["./src/lib/generated/**"],
   javascript: {
     overrides: {
       "node/no-unsupported-features/node-builtins": [
         "error",
         { allowExperimental: true },
       ],
-    },
-  },
-  tailwindcss: {
-    overrides: {
-      "tailwindcss/no-unknown-classes": "off",
     },
   },
 });
